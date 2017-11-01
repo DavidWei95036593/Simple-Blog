9ea6a6be0b9f485743fbb8683fe3306a8e6288f3
DavidWei95036593 <dw36593@pausd.us>
2017-11-01 13:49:33 -0700

    make the driver code for index

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log("hi");
+var button = document.getElementById("createblog");
+button.addEventListener("click",function(){
+
+
+
+var input = document.getElementById("text")
+var newP = document.createElement("p");
+newP.innerHTML = input.value;
+var section = document.getElementById("main");
+var deleteButton = document.createElement("button");
+deleteButton.innerHTML = "Delete";
+section.append(newP);
+section.append(deleteButton);
+
+})
