6ae10e9f28c5590d6784ec4ccd1c52113439cc34
DavidWei95036593 <dw36593@pausd.us>
2017-11-02 12:11:55 -0700

    Jack is a dick

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 var button = document.getElementById("createblog");
 button.addEventListener("click",function(){
+
 var input = document.getElementById("text")
 var newP = document.createElement("p");
 newP.innerHTML = input.value;
@@ -9,10 +10,13 @@ var newT = document.createElement("t");
 newT.innerHTML = title.value;
 
 var section = document.getElementById("main");
+var head = document.getElementById("head");
+
 section.append(newT);
 section.append(newP);
 input.value = "";
 title.value = "";
+
 var deleteButton = document.createElement("button");
 deleteButton.innerHTML = "Delete";
 deleteButton.addEventListener("click",function(){
