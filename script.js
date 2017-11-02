0ef952ccc9a2738423a1e29a5f8705b6858e3ba0
DavidWei95036593 <dw36593@pausd.us>
2017-11-02 09:28:00 -0700

    make the delete button for title

--- script.js
+++ script.js
@@ -1,19 +1,21 @@
 var button = document.getElementById("createblog");
 button.addEventListener("click",function(){
-
-
-
 var input = document.getElementById("text")
 var newP = document.createElement("p");
 newP.innerHTML = input.value;
+
 var title = document.getElementById("title");
 var newT = document.createElement("t");
 newT.innerHTML = title.value;
+
 var section = document.getElementById("main");
-var deleteButton = document.createElement("button");
-deleteButton.innerHTML = "Delete";
 section.append(newT);
 section.append(newP);
+
+var deleteButton = document.createElement("button");
+deleteButton.innerHTML = "Delete";
+deleteButton.addEventListener("click",function(){
+	section.removeChild(newT)})
 section.append(deleteButton);
 
 })
