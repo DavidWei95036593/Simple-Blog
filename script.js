f74c0f7a7bfe5e35f089bfc1ee3b82f450127e43
DavidWei95036593 <dw36593@pausd.us>
2017-11-02 09:30:35 -0700

    When click create Blog the title and paragraph will be clear

--- script.js
+++ script.js
@@ -11,7 +11,8 @@ newT.innerHTML = title.value;
 var section = document.getElementById("main");
 section.append(newT);
 section.append(newP);
-
+input.value = "";
+title.value = "";
 var deleteButton = document.createElement("button");
 deleteButton.innerHTML = "Delete";
 deleteButton.addEventListener("click",function(){
