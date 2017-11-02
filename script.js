5552124d9e2d73296681c73a4f4cb84cc5ccb313
DavidWei95036593 <dw36593@pausd.us>
2017-11-02 08:22:20 -0700

    create title

--- script.js
+++ script.js
@@ -6,9 +6,13 @@ button.addEventListener("click",function(){
 var input = document.getElementById("text")
 var newP = document.createElement("p");
 newP.innerHTML = input.value;
+var title = document.getElementById("title");
+var newT = document.createElement("t");
+newT.innerHTML = title.value;
 var section = document.getElementById("main");
 var deleteButton = document.createElement("button");
 deleteButton.innerHTML = "Delete";
+section.append(newT);
 section.append(newP);
 section.append(deleteButton);
 
