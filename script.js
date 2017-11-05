11bdc2f0e8f7e4d94daef131f22e3b8cf3ffa44e
DavidWei95036593 <dw36593@pausd.us>
2017-11-05 11:21:28 -0800

    create error message

--- script.js
+++ script.js
@@ -4,15 +4,26 @@ button.addEventListener("click",function(){
 var input = document.getElementById("textarea")
 var newP = document.createElement("p");
 newP.innerHTML = input.value;
+var section = document.getElementById("main");
 
 var title = document.getElementById("maintitle");
 var newT = document.createElement("t");
 newT.innerHTML = title.value;
+var t1 = document.getElementById("head");
 
-var section = document.getElementById("main");
-var head = document.getElementById("head");
+var exception = document.getElementById("error");
+var	Ex = document.createElement("E");
+Ex.innerHTML = exception.value
+var E = document.getElementById("error")
 
-head.append(newT);
+
+if(input.value === "" && title.value ===""){
+	console.log("hi")
+	Ex.append("hi");
+
+}
+	else{
+t1.append(newT);
 section.append(newP);
 
 input.value = "";
@@ -21,9 +32,9 @@ title.value = "";
 var deleteButton = document.createElement("button");
 deleteButton.innerHTML = "Delete";
 deleteButton.addEventListener("click",function(){
-	head.removeChild(newT);
+	t1.removeChild(newT);
 	section.removeChild(newP);
 	section.removeChild(deleteButton);})
   section.append(deleteButton);
-
+}
 })
