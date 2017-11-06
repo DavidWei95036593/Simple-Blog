0192fb238cdf59efdcb9f05b51c620067d73a87b
DavidWei95036593 <dw36593@pausd.us>
2017-11-06 13:38:54 -0800

    make some new functions in the simple blog

--- script.js
+++ script.js
@@ -36,3 +36,59 @@ deleteButton.addEventListener("click",function(){
   section.append(deleteButton);
 }
 })
+
+
+
+
+
+function hideAll(){
+  const sections = document.querySelectorAll("section.tabs");
+  for(let i = 0; i < sections.length; i++){
+    if(!sections[i].className.match(" hidden")){
+        sections[i].className += " hidden";
+      }
+    }
+}
+
+function unhide(element){
+  element.className = element.className.replace(" hidden", "");
+
+}
+
+function bindEventListeners() {
+  let menuItems = document.querySelectorAll("nav button");
+  for(let i = 0; i < menuItems.length; i++){
+    menuItems[i].addEventListener("click",function(){
+      hideAll();
+      const className = menuItems[i].className;
+      const section = document.querySelector("section." + className);
+      unhide(section);
+    })
+  }
+}
+
+
+
+
+bindEventListeners();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
